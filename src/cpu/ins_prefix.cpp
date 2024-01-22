7e5eaac957eecab29d1f475dbb16a8e02c1bdbec
ady <adityapandia18@gmail.com>
2024-01-22 22:30:43 +0530

    Added SET instructions

--- src/cpu/ins_prefix.cpp
+++ src/cpu/ins_prefix.cpp
@@ -6,7 +6,42 @@
 
 //SECTION - PREFIX INSTRUCTIONS
 
+void CPU::PFX_SET_HL(){};
+void CPU::PFX_SET_R8(){};
+
+
 void CPU::PFX()
 {
   read_ins();
+
+  
+  if (op & 0b11000000 != 0b00000000){ // Check if first 2 bits are 11
+    pfx_register_index = op & 0b00000111;
+    pfx_bit_index = (op & 0b00111000) >> 3;
+    
+    if(pfx_register_index == 0b110){  // HL
+      PFX_SET_HL();
+      cycles = (3+1);
+    }
+
+    if(pfx_register_index != 0b110){
+      pfx_working_register = pfx_register_operands_map[pfx_register_index];
+      PFX_SET_R8;
+      cycles = (2+1);
+    }
+
+  }
+
+}
+
+void CPU::PFX_SET_HL(){
+    operand = read8(HL.full);
+    operand_addr = HL.full;
+    operand |= (pfx_bit_index << 1);
+    write8(operand_addr, operand);
+
+  }
+
+void CPU::PFX_SET_R8(){
+  *pfx_working_register |= (pfx_bit_index << 1);
 }
