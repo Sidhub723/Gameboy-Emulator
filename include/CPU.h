0df515e1f3aea9910fbb209b61566c706809a912
Madhav Dhingra <madhavdhingra11@gmail.com>
2024-01-22 15:44:50 +0530

    Added SLA functions

--- include/CPU.h
+++ include/CPU.h
@@ -62,6 +62,8 @@ private:
 //!SECTION Prefix Instructions
 private:
   void PFX();
+  void SLA(); // Shift Left Arithmetically for r8
+  void SLA_HL(); //Shift Left Arithmetically for HL
 
 //SECTION - Instructions
 private:
--- src/cpu/ins_prefix.cpp
+++ src/cpu/ins_prefix.cpp
@@ -9,4 +9,60 @@
 void CPU::PFX()
 {
   read_ins();
+
+  uint8_t prefix_family = op & 0b11000000;
+  uint8_t exact_function = op & 0b00111000;
+  uint8_t register_family = op & 0b00000111;
+  if(prefix_family == 0b00)
+  {
+    if(exact_function == 0b100) 
+    {
+      if(register_family == 0b100 || register_family == 0b101)
+        CPU::SLA_HL();
+      else
+        CPU::SLA();
+
+    }
+  }
+
 }
+
+void CPU::SLA()
+{
+  uint8_t register_family = op & 0b00000111;
+  uint8_t *reg = pfx_register_operands_map[register_family];
+  int cycles = 2;
+  while(cycles--)
+  {
+    set_flag(Flags::neg, 0);
+    set_flag(Flags::half_carry, 0);
+    if((*reg) & 0b10000000)
+      set_flag(Flags::carry, 1);
+    else
+      set_flag(Flags::carry, 0);
+    *reg = (*reg<<1);
+    if(*reg==0)
+      set_flag(Flags::zero, 1);
+  }
+
+}
+
+void CPU::SLA_HL()
+{
+  uint8_t register_family = op & 0b00000111;
+  uint8_t *reg = pfx_register_operands_map[register_family];
+  int cycles = 4;
+  while(cycles--)
+  {
+    set_flag(Flags::neg, 0);
+    set_flag(Flags::half_carry, 0);
+    if((*reg) & 0b10000000)
+      set_flag(Flags::carry, 1);
+    else
+      set_flag(Flags::carry, 0);
+    *reg = (*reg<<1);
+    if(*reg==0)
+      set_flag(Flags::zero, 1);
+  }
+}
+
